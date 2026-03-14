52e902fe9637e7cbab91da244124227365ae8446
Pankaj Sillay <pankajsillay2003@gmail.com>
2026-03-15 03:00:54 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pankaj Sillay",
+    author_email="pankajsillay2003@gmail.com",
+    packages=find_packages()
+)
